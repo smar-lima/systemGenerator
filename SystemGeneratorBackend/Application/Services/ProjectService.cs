0c7bdc7ab076e18ae12f4756366d94b01e159640
Fabio Almeida Costa <40249761+fabioalmcosta@users.noreply.github.com>
2022-05-13 12:09:42 -0300

    Crud Project

--- SystemGeneratorBackend/Application/Services/ProjectService.cs
+++ SystemGeneratorBackend/Application/Services/ProjectService.cs
@@ -17,15 +17,15 @@ namespace Application.Services
             _projectRepository = userRepository;
         }
 
-        public async Task<long> Post(PostProjectModel model)
+        public async Task<Project> Post(PostPutProjectModel model)
         {
-            ValidateProject(model.Name, model.Location);
+            await ValidateProject(model.Name, model.Location);
 
             Project newProject = new Project(model.Name, model.Location);
 
             _projectRepository.Insert(newProject);
 
-            return newProject.Id;
+            return newProject;
         }
 
 
@@ -35,13 +35,42 @@ namespace Application.Services
 
             return project;
         }
-        private async void ValidateProject(string name, string location)
+
+        public async Task<Project> Put(long id, PostPutProjectModel model)
+        {
+            var project = await ValidateExistingProject(id);
+            await ValidateProject(model.Name, model.Location, id);
+
+            project.Name = model.Name;
+            project.Location = model.Location;
+
+            _projectRepository.Update(project);
+
+            return project;
+        }
+
+        public async Task Delete(long id)
+        {
+            var project = await ValidateExistingProject(id);
+            _projectRepository.Delete(project);
+        }
+
+        private async Task ValidateProject(string name, string location, long? id = null)
         {
             var existingProject = await (from projects in _projectRepository.GetAllReadOnly()
-                                         where projects.Name == name || projects.Location == location
-                                         select projects).ToListAsync();
+                                   where (projects.Name == name || projects.Location == location) && projects.Id != id
+                                   select projects).FirstOrDefaultAsync();
 
             if (existingProject != null) throw new HttpStatusException(HttpStatusCode.BadRequest, "Project name or location already exists!");
+        }
+
+        private async Task<Project> ValidateExistingProject(long id)
+        {
+            var existingProject = await _projectRepository.GetByIdAsync(id);
+
+            if (existingProject == null) throw new HttpStatusException(HttpStatusCode.BadRequest, "Project does not exists!");
+
+            return existingProject;
         }
     }
 }
---
+++ SystemGeneratorBackend/Domain/Enums/Base/OperationTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Enums.Base
+{
+    public enum OperationTypeEnum
+    {
+        Post = 1,
+        Put = 2,
+        Delete = 3,
+        Get = 4,
+        MassDelete = 5,
+        MassGet = 6,
+    }
+}
